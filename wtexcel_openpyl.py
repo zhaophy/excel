efff0955ae46bf7cdd9f7fc33e383de1290a9521
zhaophy <757308024@qq.com>
2019-02-21 17:14:49 +0800

    openpyxl

--- wtexcel_openpyl.py
+++ wtexcel_openpyl.py
@@ -9,6 +9,11 @@ ws = wb.active
 data_a = [{'name':'echo_one','age':'56','rank':'A','Class':'43'},
           {'age':'44','rank':'B+','Class':'9','name':'echo_four','phone':'10086','add':'henan'},
           {'age':'47','rank':'A','Class':'9'}]
+data_b = [{'name':'echo_one','age':'51','rank':'A','Class':'43'},
+          {'age':'42','rank':'B+','Class':'9','name':'echo_four','phone':'10086','add':'henan'},
+          {'age':'47','rank':'A','Class':'re564563465435643655435634'}]
+u = data_a + data_b
+def get_v(data):
     keys = data[0].keys() #列表中第一个字典的所有key组成的集合
     for i in data:
         keys += i.keys()
@@ -22,9 +27,12 @@ data_a = [{'name':'echo_one','age':'56','rank':'A','Class':'43'},
             except:
                 v.append(None)
         vs.append(v)  #列表所有各字典的value组成的列表
-    return ([newkeys] + [[]] + vs)  #key 和 value组成的新列表
-for i in get_v(data_a):
+    return ([newkeys] + [[]] + vs)  #key 和 B
+for i in get_v(u):
     print i
-    ws.append(i)  #写入excel
+    ws.append(i)
 
 wb.save('a.xlsx')
+
+
+
